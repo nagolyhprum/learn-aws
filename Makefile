5d0045cb1f07e3cab2963b3823461c18c646f22f
Logan Murphy <lmurphy@learnvest.com>
2018-06-13 18:23:42 -0400

    just a little demo of ts

--- Makefile
+++ Makefile
@@ -1,9 +1,7 @@
-build-fe:
-	npm run parcel -- build client/index.html --out-dir clientBuild
-
-build-be:  
+prod-fe:
+  npm run parcel -- build client/index.html --out-dir clientBuild
+prod-be:
   npm run parcel -- build server/index.ts --out-dir serverBuild --target node
   npm run forever -- stopall
   npm run forever -- -l forever.log -o out.log -e err.log -a start serverBuild/index.js
-
-start: build-fe build-be
+prod: prod-fe prod-be
--- client/index.ts
+++ client/index.ts
@@ -1,3 +1,30 @@
+enum Direction {
+    add, remove, update
+}
+
+interface Event {
+  type : Direction
+}
+
+interface Context {
+  fillRect(x:string,y:string,w:string,h:string);
+}
+
 const canvas = document.querySelector("canvas");
-const context = canvas.getContext("2d");
+const context : Context = canvas.getContext("2d");
 context.fillRect(0, 0, canvas.width, canvas.height);
+
+const add : Event = {
+  type : Direction.add
+}
+console.log(add);
+
+const remove : Event = {
+  type : Direction.remove
+}
+console.log(remove);
+
+const update : Event = {
+  type : Direction.update
+}
+console.log(update);
